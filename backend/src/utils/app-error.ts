e1c4dbb5e6a94ebf4559d02013c2113e20a2de3c
charan11w <charan11w@gmail.com>
2026-08-26 15:11:27 +0530

    feat: create AppError class for structured error handling

---
+++ backend/src/utils/app-error.ts
@@ -0,0 +1,16 @@
+
+
+export class AppError extends Error{
+  statusCode:number;
+  code:string;
+
+  constructor(message:string,statusCode:number,code:string){
+    //initiate message in the parent;
+    super(message);
+
+
+    this.statusCode=statusCode;
+    this.code=code
+  }
+
+}
